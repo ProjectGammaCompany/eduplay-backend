d2fd8c43b978ee2fe1262b0360e0502ca473491a
Victoria Khan <vshkhan@edu.hse.ru>
2026-01-29 12:35:01 +0300

    Update Credentials.go

--- eduplay-gateway/internal/lib/models/user/Credentials.go
+++ eduplay-gateway/internal/lib/models/user/Credentials.go
@@ -3,8 +3,8 @@ package userModel
 type Role string
 
 type Credentials struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
 	// Role         Role   `json:"role"`
 	// AccessLevel  int64  `json:"access_level"`
 }
