972241f372fa578b031194f4a34f71629c1b1902
Victoria Khan <vshkhan@edu.hse.ru>
2026-04-21 12:15:55 +0300

    eduplay mail fix

--- eduplay-mail/run.py
+++ eduplay-mail/run.py
@@ -6,4 +6,4 @@ load_dotenv()
 PORT = os.getenv('PORT')
 
 if __name__ == "__main__":
-    app.run(hort="0.0.0.0", port=int(PORT), debug=True)
+    app.run(host="0.0.0.0", port=int(PORT), debug=True)
